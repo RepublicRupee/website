358e2eeca07ae6beaaed7ea2b96197ae86361c08
Rajesh <93503521+TheRepublicCoin@users.noreply.github.com>
2021-11-02 12:07:27 +0800

    react

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <code>
+          <h1>Republic Rupee</h1>
+          Coming Soon! India's Blockchain Powered Stablecoin.
+        </code>
+        <br></br>
+        <code>
+        <a
+          className="App-link"
+          href="https://t.me/RepublicRupee"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Join Telegram
+        </a> | <a
+          className="App-link"
+          href="https://twitter.com/RepublicRupee"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Follow Twitter
+        </a>
+        </code>
+      </header>
+    </div>
+  );
+}
+
+export default App;
